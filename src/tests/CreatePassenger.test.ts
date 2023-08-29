93b6a8c2d05a0b503b7c7f4992a8898040812672
vlduggen <vitor.duggen@amaiseducacao.com.br>
2023-08-29 13:33:33 -0300

    Criado teste de integração do CreatePassenger

---
+++ src/tests/CreatePassenger.test.ts
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+test('Deve criar uma passageiro e retornar o passenger_id', async function () {
+    const { data } = await axios.post('http://127.0.0.1:3000/passengers', {
+        name: "Vitor Luiz Duggen",
+        email: "test@gmail.com",
+        document: "70106501011"
+    });
+    expect(data).toHaveProperty('passenger_id');
+})
