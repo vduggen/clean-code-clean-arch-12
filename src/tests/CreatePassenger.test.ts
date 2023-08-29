5cb8a621e638a4362093078430acda083cc12239
vlduggen <vitor.duggen@amaiseducacao.com.br>
2023-08-29 13:37:01 -0300

    Ajustado CreatePassenger.test.ts

--- src/tests/CreatePassenger.test.ts
+++ src/tests/CreatePassenger.test.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-test('Deve criar uma passageiro e retornar o passenger_id', async function () {
+test('Deve criar um passageiro e retornar o passenger_id', async function () {
     const { data } = await axios.post('http://127.0.0.1:3000/passengers', {
         name: "Vitor Luiz Duggen",
         email: "test@gmail.com",
