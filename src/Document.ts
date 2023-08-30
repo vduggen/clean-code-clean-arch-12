8a776ab7eb239ba939ce1cbdc589748372273f75
vitor.duggen-at-410124394847 <vitor.duggen-at-410124394847>
2023-08-30 20:43:14 -0300

    Criado constantes e separado em métodos e ajustado testes

--- src/Document.ts
+++ src/Document.ts
@@ -1,40 +1,92 @@
 export class Document {
+    private readonly MINIMUN_LENGTH = 11;
+    private readonly MAXIMUM_LENGTH = 14;
+    private readonly DOCUMENT_WITHOUT_LAST_DIGIT_LENGHT = 11;
+
+    constructor(
+        private readonly document: unknown
+    ) {
+        this.validate();
+    }
+
     private isValidLength(str: any) {
-        return str.length >= 11 && str.length <= 14;
+        return str.length >= this.MINIMUN_LENGTH && str.length <= this.MAXIMUM_LENGTH;
     }
 
-    private allDigitsSame(document: string) {
+    private allDigitsSame(document: unknown) {
+        const digitsDocument = String(document).split("");
+        const firstDigit = digitsDocument[0];
+        return digitsDocument.every(currentDigit => currentDigit === firstDigit);
+    }
+
+    private getDigitByRestDivision(restDivision: number) {
+        return restDivision < 2 ? 0 : this.DOCUMENT_WITHOUT_LAST_DIGIT_LENGHT - restDivision;
+    }
+
+    private getRestDivisionDigitBySumDigits(sumDigits: number) {
+        return sumDigits % this.DOCUMENT_WITHOUT_LAST_DIGIT_LENGHT;
+    }
+
+    private getTotalAllDigits(document: string) {
+        const sizeDocumentWithTwoDigits = document.length + 2;
+        let total = 0;
         const digitsDocument = document.split("");
-        const firstDigit = document[0];
-        return digitsDocument.every(digit => digit === firstDigit);
+        digitsDocument.forEach((currentDigit, index) => {
+            const indexIncremented = index + 1;
+            const currentNumberMultiplication = sizeDocumentWithTwoDigits - indexIncremented;
+            const currentDigitNumber = +currentDigit;
+            const sum = currentNumberMultiplication * currentDigitNumber;
+            total += sum;
+        });
+        return total;
     }
 
-    private getDigitByRest(rest: number) {
-        return rest < 2 ? 0 : 11 - rest;
+    private getDocumentWithoutTwoLastDigits(document: string) {
+        return document.substring(0, document.length - 2);
     }
 
-    // @ts-ignore
-    public validate (str) {
-        if (!str) return false;
-        if (!this.isValidLength(str)) return false;
-        str = str.replace(/\D/g, '');
-        if (this.allDigitsSame(str)) return false;
+    private getFirstDigit(document: string) {
+        const documentWithoutTwoLastDigits = this.getDocumentWithoutTwoLastDigits(document);
+        const sumFirstDigit = this.getTotalAllDigits(documentWithoutTwoLastDigits);
+        const restDivision = this.getRestDivisionDigitBySumDigits(sumFirstDigit);
+        return this.getDigitByRestDivision(restDivision);
+    }
+
+    private getSecondDigit(document: string, firstDigit: number) {
+        const documentWithoutTwoLastDigits = this.getDocumentWithoutTwoLastDigits(document);
+        const documentWithNewFirstDigit = `${documentWithoutTwoLastDigits}${firstDigit}`;
+        const sumSecondDigit = this.getTotalAllDigits(documentWithNewFirstDigit);
+        const restDivision = this.getRestDivisionDigitBySumDigits(sumSecondDigit)
+        return this.getDigitByRestDivision(restDivision);
+    }
+
+    private getTwoLastDigitsCalculated(document: string) {
+        const firstDigit = this.getFirstDigit(document);
+        const secondDigit = this.getSecondDigit(document, firstDigit);
+        return `${firstDigit}${secondDigit}`;
+    }
+
+    private getLastTwoDigitsIsValid(document: string) {
+        const twoLastDigitsCalculated = this.getTwoLastDigitsCalculated(document);
+        const oldTwoLastDigits = document.substring(document.length - 2);
+        return twoLastDigitsCalculated === oldTwoLastDigits;
+    }
+
+    private getDocument() {
+        return String(this.document);
+    }
+
+    private getFormatted() {
+        return this.getDocument().replace(/\D/g, '');
+    }
+
+    public validate() {
+        if (!this.document) return false;
+        if (!this.isValidLength(this.document)) return false;
+        const documentFormatted = this.getFormatted();
+        if (this.allDigitsSame(documentFormatted)) return false;
         try {
-            let d1 = 0;
-            let d2 = 0;
-            for (let nCount = 1; nCount < str.length -1; nCount++) {  
-                const digito = parseInt(str.substring(nCount -1, nCount));
-                d1 += (11 - nCount) * digito;
-                d2 += (12 - nCount) * digito;
-            };  
-            const rest1 = (d1 % 11);
-            const firstDigit = this.getDigitByRest(rest1);
-            d2 += 2 * firstDigit;  
-            const rest2 = (d2 % 11);  
-            const twoLastDigits = str.substring(str.length-2, str.length);  
-            const secondDigit = this.getDigitByRest(rest2);
-            const nDigResult = `${firstDigit}${secondDigit}`;
-            return twoLastDigits == nDigResult;
+            return this.getLastTwoDigitsIsValid(documentFormatted);
         }catch (e){  
             console.error("Erro !"+e);  
             return false;  
--- src/tests/Document.test.ts
+++ src/tests/Document.test.ts
@@ -2,47 +2,47 @@ import { Document } from "../Document";
 
 describe('Document', function() {
     test('Deve retornar false se for enviado um valor null', function() {
-        const document = new Document();
-        expect(document.validate(null)).toBeFalsy();
+        const document = new Document(null);
+        expect(document.validate()).toBeFalsy();
     })
 
     test('Deve retornar undefined se for enviado undefined', function() {
-        const document = new Document();
-        expect(document.validate(undefined)).toBeFalsy();
+        const document = new Document(undefined);
+        expect(document.validate()).toBeFalsy();
     })
 
     test('Caso o tamanho da string for menor que 11 deve retornar false', function() {
-        const document = new Document();
-        expect(document.validate("123.456")).toBeFalsy();
+        const document = new Document("123.456");
+        expect(document.validate()).toBeFalsy();
     })
 
     test('Caso o tamanho da string for maior que 14 deve retornar false', function() {
-        const document = new Document();
-        expect(document.validate("123.456.789.012.345.67")).toBeFalsy();
+        const document = new Document("123.456.789.012.345.67");
+        expect(document.validate()).toBeFalsy();
     })
 
     test('Caso os números do documento forem repetidos deve retornar false', function() {
-        const document = new Document();
-        expect(document.validate("33333333333333")).toBeFalsy();
+        const document = new Document("33333333333333");
+        expect(document.validate()).toBeFalsy();
     })
 
     test('CPF com primeiro dígito 0', function() {
-        const document = new Document();
-        expect(document.validate("123.456.789-00")).toBeFalsy();
+        const document = new Document("123.456.789-00");
+        expect(document.validate()).toBeFalsy();
     })
 
     test('CPF com segundo dígito 0', function() {
-        const document = new Document();
-        expect(document.validate("987.654.321-98")).toBeFalsy();
+        const document = new Document("987.654.321-98");
+        expect(document.validate()).toBeFalsy();
     })
 
     test('CPF Inválido', function() {
-        const document = new Document();
-        expect(document.validate("111.444.777-05")).toBeFalsy();
+        const document = new Document("111.444.777-05");
+        expect(document.validate()).toBeFalsy();
     })
 
     test('CPF Válido', function() {
-        const document = new Document();
-        expect(document.validate("092.216.699-47")).toBeTruthy();
+        const document = new Document("092.216.699-47");
+        expect(document.validate()).toBeTruthy();
     })
 })
